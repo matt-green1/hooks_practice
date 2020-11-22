f9e1197232541d27e28665265f1d2883d65a05bd
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-11-22 17:22:17 -0500

    refactored as two seperate state variables

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [{ count, count2 }, setCount] = useState({count: 10, count2: 20})
+  const [ count, setCount ] = useState(10)
+  const [ count2, setCount2 ] = useState(20)
+
   return (
     <div>
-      <button onClick={() => 
-        setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))
+      <button
+        onClick={() => {
+          setCount(c => c + 1);
+          setCount2(c => c + 1)
+      }}
+        > Add 1</button>
       
-      }> Add 1</button>
+
       <div> count 1: {count}</div>
       <div> count 2: {count2}</div>
 
