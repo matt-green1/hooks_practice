53a4b3f139fc28d8e994f3c24a43d35e5571b845
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-11-22 17:05:23 -0500

    two values in state as an object before refactor

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(10)
+  const [{ count, count2 }, setCount] = useState({count: 10, count2: 20})
   return (
     <div>
-      <button onClick={() => setCount(currentCount => currentCount + 1)}> Add 1</button>
-      <div>{count}</div>
+      <button onClick={() => 
+        setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))
+      
+      }> Add 1</button>
+      <div> count 1: {count}</div>
+      <div> count 2: {count2}</div>
+
     </div>
   )
 }
