526806e7cd354fd9a5466cfcfe1550393ae86ef2
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-11-22 16:26:22 -0500

    basic functional component with useState

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(10)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setCount(currentCount => currentCount + 1)}> Add 1</button>
+      <div>{count}</div>
     </div>
-  );
+  )
 }
 
 export default App;
