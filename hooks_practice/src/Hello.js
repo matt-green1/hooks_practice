a02cb9e353b75c1360a3b2360f7728e27080c3bb
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-11-24 22:18:46 -0500

    notes added

--- hooks_practice/src/Hello.js
+++ hooks_practice/src/Hello.js
@@ -4,6 +4,7 @@ export const Hello = () => {
   React.useEffect(() => {
     console.log("render");
 
+    //cleanup function (stand in for an unmount here)
     return () => {
       console.log("unmount");
     };
